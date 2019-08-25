c3cc4a6f66d35c71f34e7abafe5d9f4157f9707a
Eleanor Palmer <ePalmer@hhogdev.com>
2019-08-25 11:38:05 -0400

    Completed unit tests for fake person repository.

---
+++ PeopleSearch.Tests/PersonRepositoryTests.cs
@@ -0,0 +1,153 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PeopleSearch.Models;
+using PeopleSearch.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PeopleSearch.Tests
+{
+    [TestClass]
+    public class PersonRepositoryTests
+    {
+        PersonRepositoryFake _repository;
+
+        public PersonRepositoryTests()
+        {
+            _repository = new PersonRepositoryFake();
+        }
+
+        [TestMethod]
+        public void Get_ReturnsNameAgeImageUrl()
+        {
+            Person personTest = _repository.Get(1);
+            Person person = new Person();
+            person.PersonId = 1;
+            person.FirstName = "Bruce";
+            person.LastName = "Wayne";
+            person.Age = 37;
+            person.ImageUrl = "~/images/bwayne.jpg";
+
+            Assert.AreEqual(person.FirstName, personTest.FirstName);
+            Assert.AreEqual(person.LastName, personTest.LastName);
+            Assert.AreEqual(person.Age, personTest.Age);
+            Assert.AreEqual(person.ImageUrl, personTest.ImageUrl); ;
+        }
+
+        [TestMethod]
+        public void Get_ReturnsInterests()
+        {
+            List<PersonInterest> interests = _repository.GetInterests(1);
+            var originalInterests = new List<string>();
+
+            foreach (var item in interests)
+            {
+                originalInterests.Add(item.Interest.Title);
+            }
+
+            Assert.AreEqual(originalInterests.Contains("Books"), true);
+            Assert.AreEqual(originalInterests.Contains("Sports"), true);
+        }
+
+        [TestMethod]
+        public void Get_ReturnsAddress()
+        {
+            Person personTest = _repository.Get(1);
+            var originalAddress = personTest.Address;
+            var testAddress = new Address
+            {
+                Id = 1,
+                Address1 = "1000 Wayne Manor Dr.",
+                City = "Gotham",
+                State = "NJ",
+                ZipCode = 22222
+            };
+
+            Assert.AreEqual(originalAddress.Address1, testAddress.Address1);
+            Assert.AreEqual(originalAddress.Address2, testAddress.Address2);
+            Assert.AreEqual(originalAddress.City, testAddress.City);
+            Assert.AreEqual(originalAddress.State, testAddress.State);
+            Assert.AreEqual(originalAddress.ZipCode, testAddress.ZipCode);
+        }
+
+        [TestMethod]
+        public void Get_InvalidId_ReturnsNull()
+        {
+            Assert.AreEqual(_repository.Get(7), null);
+        }
+
+        [TestMethod]
+        public void GetAll_ReturnsAll()
+        {
+            var persons = _repository.GetAll();
+
+            Assert.AreEqual(persons.Count, 2);
+        }
+
+        [TestMethod]
+        public void Add_CreatesNewPerson()
+        {
+            var person = new Person
+            {
+                PersonId = 3,
+                FirstName = "Diana",
+                LastName = "Prince",
+                Age = 54,
+                AddressId = 3,
+                ImageUrl = "~/images/dprince.jpg"
+            };
+
+            var address = new Address
+            {
+                Id = 3,
+                Address1 = "1200 Main Ave.",
+                Address2 = "Suite 300",
+                City = "New York City",
+                State = "NY",
+                ZipCode = 72172
+            };
+
+            var interests = new List<Interest>() {
+                new Interest() { InterestId = 2 },
+                new Interest() { InterestId = 3 }
+            };
+
+            _repository.Add(person, address, interests);
+            var persons = _repository.GetAll();
+
+            Assert.AreEqual(persons.Count, 3);
+        }
+
+        [TestMethod]
+        public void Add_InvalidValues_DoesNotCreateNewPerson()
+        {
+            var person = new Person
+            {
+                PersonId = 4,
+                Age = 27,
+                AddressId = 4,
+                ImageUrl = "~/images/bgordon.jpg"
+            };
+
+            var address = new Address
+            {
+                Id = 4,
+                Address2 = "Suite 300",
+                City = "New York City",
+                State = "NY",
+                ZipCode = 72172
+            };
+
+            var interests = new List<Interest>() {
+                new Interest() { InterestId = 2 },
+                new Interest() { InterestId = 3 }
+            };
+
+            var newPerson = _repository.Add(person, address, interests);
+            var persons = _repository.GetAll();
+
+            Assert.AreEqual(newPerson, null);
+            Assert.AreEqual(persons.Count, 2);
+        }
+    }
+}
--- PeopleSearch/Repositories/PersonRepositoryFake.cs
+++ PeopleSearch/Repositories/PersonRepositoryFake.cs
@@ -9,24 +9,150 @@ namespace PeopleSearch.Repositories
     public class PersonRepositoryFake
     {
         private readonly List<Person> _persons;
+        private readonly List<Address> _addresses;
+        private readonly List<Interest> _interests;
+        private readonly List<PersonInterest> _personinterests;
 
         public PersonRepositoryFake()
         {
+            _interests = new List<Interest>()
+            {
+                new Interest
+                {
+                    InterestId = 1,
+                    Title = "Books"
+                },
+                new Interest
+                {
+                    InterestId = 2,
+                    Title = "Writing"
+                },
+                new Interest
+                {
+                    InterestId = 3,
+                    Title = "Movies"
+                },
+                new Interest
+                {
+                    InterestId = 4,
+                    Title = "Sports"
+                },
+                new Interest
+                {
+                    InterestId = 5,
+                    Title = "Hiking"
+                }
+            };
+
+            _addresses = new List<Address>()
+            {
+                new Address
+                {
+                    Id = 1,
+                    Address1 = "1000 Wayne Manor Dr.",
+                    City = "Gotham",
+                    State = "NJ",
+                    ZipCode = 22222
+                },
+                new Address
+                {
+                    Id = 2,
+                    Address1 = "404 7th St.",
+                    Address2 = "Apt. 603",
+                    City = "National City",
+                    State = "WA",
+                    ZipCode = 54545
+                }
+            };
+
             _persons = new List<Person>()
             {
-                new Person() { PersonId = 1,
-                    FirstName = "Bruce", LastName = "Wayne"}
+                new Person()
+                {
+                    PersonId = 1,
+                    FirstName = "Bruce",
+                    LastName = "Wayne",
+                    Age = 37,
+                    Address = _addresses.Where(x => x.Id == 1).FirstOrDefault(),
+                    ImageUrl = "~/images/bwayne.jpg"
+                },
+                new Person
+                {
+                    PersonId = 2,
+                    FirstName = "Kara",
+                    LastName = "Danvers",
+                    Age = 22,
+                    Address = _addresses.Where(x => x.Id == 2).FirstOrDefault(),
+                    ImageUrl = "~/images/kdanvers.jpg"
+                }
+            };
+
+            _personinterests = new List<PersonInterest>()
+            {
+                new PersonInterest
+                {
+                    PersonId = 1,
+                    InterestId = 1,
+                    Interest = _interests.Where(x => x.InterestId == 1).FirstOrDefault()
+                },
+                new PersonInterest
+                {
+                    PersonId = 1,
+                    InterestId = 4,
+                    Interest = _interests.Where(x => x.InterestId == 4).FirstOrDefault()
+                },
+                new PersonInterest
+                {
+                    PersonId = 2,
+                    InterestId = 2,
+                    Interest = _interests.Where(x => x.InterestId == 2).FirstOrDefault()
+                },
+                new PersonInterest
+                {
+                    PersonId = 2,
+                    InterestId = 3,
+                    Interest = _interests.Where(x => x.InterestId == 3).FirstOrDefault()
+                }
             };
         }
 
-        public Person Add(Person person, Address address, List<Interest> interests)
+        public Person Get(int id)
         {
-            return person;
+            return _persons.Where(x => x.PersonId == id).FirstOrDefault();
+        }
+
+        public List<PersonInterest> GetInterests(int id)
+        {
+            return _personinterests.Where(x => x.PersonId == id).ToList<PersonInterest>();
         }
 
         public List<Person> GetAll()
         {
             return _persons;
+        }
+
+        public Person Add(Person person, Address address, List<Interest> interests)
+        {
+            person.Address = address;
+
+            foreach(var item in interests)
+            {
+                _personinterests.Add(new PersonInterest()
+                {
+                    PersonId = person.PersonId,
+                    InterestId = item.InterestId,
+                    Interest = _interests.Where(x => x.InterestId == item.InterestId).FirstOrDefault()
+                });
+            }
+            
+            if(person != null && !string.IsNullOrEmpty(person.FirstName) && !string.IsNullOrEmpty(person.LastName))
+            {
+                _persons.Add(person);
+
+                return person;
+            }
+
+            return null;
         }
     }
 }
